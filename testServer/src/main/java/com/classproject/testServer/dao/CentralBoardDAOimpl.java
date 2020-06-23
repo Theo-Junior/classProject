43ef1bbfe5d52b981d2abce44376f31037ca4df6
Ella.H <63627272+MinjiHa@users.noreply.github.com>
2020-06-23 15:13:01 +0900

    0623
    board dao, impl, mapper
    member mapper

--- testServer/src/main/java/com/classproject/testServer/dao/CentralBoardDAOimpl.java
+++ testServer/src/main/java/com/classproject/testServer/dao/CentralBoardDAOimpl.java
@@ -16,32 +16,32 @@ public class CentralBoardDAOimpl implements CentralBoardDAO{
 	private SqlSession sqlSession;
 
 	@Override
-	public void insertUser(CentralBoard centralboard) throws Exception {
-		sqlSession.insert("com.classproject.testServer.dao.CentralBoardDAO.insertUser", centralboard);
+	public void insertWrite(CentralBoard centralboard) throws Exception {
+		sqlSession.insert("com.classproject.testServer.dao.CentralBoardDAO.insertWrite", centralboard);
 		// 여기에 sqlSession을 선언해서 insert메소드를 호출하고 파라미터로 centralmember를 받고있다. 
 		// 이 과정은 앞서 Service, DAO에서 호출했던 insertUser를 찾아가기 위함이다.
 		// 이제 DataAccessConfig.java로 가보자.
 	}
 
 	@Override
-	public List<CentralBoard> selectUserList() throws Exception {
-		return sqlSession.selectList("com.classproject.testServer.dao.CentralBoardDAO.selectUserList");
+	public List<CentralBoard> selectWriteList() throws Exception {
+		return sqlSession.selectList("com.classproject.testServer.dao.CentralBoardDAO.selectWriteList");
 	}
 
 	@Override
-	public CentralBoard selectUserById(int bid) throws Exception {
-		return sqlSession.selectOne("com.classproject.testServer.dao.CentralBoardDAO.selectUserById");
+	public CentralBoard selectWriteByCode(int bid) throws Exception {
+		return sqlSession.selectOne("com.classproject.testServer.dao.CentralBoardDAO.selectWriteByCode");
 	}
 
 	@Override
-	public void updateUser(CentralBoard centralboard) throws Exception {
-		sqlSession.update("com.classproject.testServer.dao.CentralBoardDAO.updateUser", centralboard);
+	public void updateWrite(CentralBoard centralboard) throws Exception {
+		sqlSession.update("com.classproject.testServer.dao.CentralBoardDAO.updateWrite", centralboard);
 		
 	}
 
 	@Override
-	public void deleteUser(int bid) throws Exception {
-		sqlSession.delete("com.classproject.testServer.dao.CentralBoardDAO.deleteUser", bid);
+	public void deleteWrite(int bid) throws Exception {
+		sqlSession.delete("com.classproject.testServer.dao.CentralBoardDAO.deleteWrite", bid);
 		
 	}
 }
